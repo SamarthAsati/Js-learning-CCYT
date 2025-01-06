0c12f803fee8f3133bf473dc5692a4875f435237
SamarthAsati <96104041+SamarthAsati@users.noreply.github.com>
2025-01-06 14:31:38 +0000

    number&Math

---
+++ 01_basics/Number.js
@@ -0,0 +1,29 @@
+// const number = 400
+// console.log(number);
+
+// const value = new Number (54000)
+// console.log(value);
+
+// console.log(value.toString().length);
+// console.log(value.toFixed(2));
+
+// const otherNumber = 24.633
+// console.log(otherNumber.toPrecision(4))
+
+// const bigNumber = 24000000000
+// console.log(bigNumber.toLocaleString('en-IN'))
+
+////////////////////////// Maths ////////////////////////////
+
+// console.log(Math)
+// console.log(Math.abs(-5343))
+// console.log(Math.round(23.733))
+// console.log(Math.ceil(23.733))
+// console.log(Math.floor(23.733))
+// console.log(Math.min(2,3,7,2,0))
+// console.log(Math.max(2,7,3,5,7))
+
+const low = 2
+const high = 6
+console.log(Math.floor(Math.random() * (high - low + 1)) + low)
+
