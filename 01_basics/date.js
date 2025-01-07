767b53b9992973e29d01d9d2ef107bee2d4a4b30
SamarthAsati <96104041+SamarthAsati@users.noreply.github.com>
2025-01-07 10:26:59 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,10 @@
+const myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+
+let newCreatedDate = new Date(2024,11,5,13,32,56)
+console.log(newCreatedDate.toLocaleString())
+
+let CreatedDate = new Date("05-01-2025 12:31")
+console.log(CreatedDate.toLocaleString())
