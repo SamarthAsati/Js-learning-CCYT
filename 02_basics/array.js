63993024380d6b5bd20fe8b45491b14974ac198c
SamarthAsati <96104041+SamarthAsati@users.noreply.github.com>
2025-01-07 14:34:44 +0000

    arraycomplete

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,2 +1,28 @@
-const myArr = new Arr(1,2,3,4,5)
+const myArr = new Array(1,2,3,4,5)
+// console.log(myArr)
+
+// const myArray = [6,7,8,9,10]
+// console.log(myArray)
+
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(4))
+// console.log(myArr.includes(64))
+
+// console.log(myArr.indexOf(5))
+// console.log(myArr.indexOf(21))
+
+// console.log(myArr.join())
+
+// Slice and Splice
+
+const myArr1 = myArr.slice(1,3)
 console.log(myArr)
+console.log(myArr1)
+
+const myArr2 = myArr.splice(1,3)
+console.log(myArr)
+console.log(myArr2)
